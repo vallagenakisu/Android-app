fd1ad421f7f85b9a350391b748e8abaac21f6a29
vallagenakisu <tanzirmannanturzo@gmail.com>
2023-11-05 01:51:30 +0600

    added firestore database

---
+++ app/src/main/java/com/example/mainprojectpersonallifetracker/Class/TodoClass.java
@@ -0,0 +1,27 @@
+package com.example.mainprojectpersonallifetracker.Class;
+
+public class TodoClass {
+    private String task;
+    private int status;
+
+    public TodoClass(String task, int status) {
+        this.task = task;
+        this.status = status;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
--- app/src/main/java/com/example/mainprojectpersonallifetracker/mainfragment/TodoFragment.java
+++ app/src/main/java/com/example/mainprojectpersonallifetracker/mainfragment/TodoFragment.java
@@ -1,7 +1,10 @@
 package com.example.mainprojectpersonallifetracker.mainfragment;
 
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
@@ -15,11 +18,14 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.mainprojectpersonallifetracker.Class.TodoClass;
 import com.example.mainprojectpersonallifetracker.MainActivity;
 import com.example.mainprojectpersonallifetracker.R;
 import com.example.mainprojectpersonallifetracker.adapters.todoadapter;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +37,7 @@ public class TodoFragment extends Fragment {
     private TextView text;
     private EditText TaskEdit;
     private List<String> mylist;
+    private DatabaseReference reference;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,20 +60,39 @@ public class TodoFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
 
-        View view1 = LayoutInflater.from(getContext()).inflate(R.layout.dialoguetodo,null);
 
+
+
+        reference = FirebaseDatabase.getInstance().getReference().child("Task");
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
+                View view1 = LayoutInflater.from(getContext()).inflate(R.layout.dialoguetodo,null);
                 AlertDialog alertDialog = new MaterialAlertDialogBuilder(getContext())
                         .setView(view1)
                         .setTitle("Add Task")
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                EditText text2 = view1.findViewById(R.id.taskadd);
+                                String task = text2.getText().toString();
+                                TodoClass obj = new TodoClass(task,0);
+
+                                reference.child(task).setValue(obj);
+
+
+                            }
+                        })
                         .create();
                 alertDialog.show();
             }
-
-            //DatabaseReference reference =  FirebaseDatabase.getInstance().getReference().child("Todo").child("Tasks");
 
 
 
